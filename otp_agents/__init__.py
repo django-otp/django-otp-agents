a41ed8799ab8acfa9f14beecff22da03403cc328
Peter Sagerson <psagers@ignorare.net>
2012-08-20 14:32:40 -0700

    django-otp-agents

---
+++ otp_agents/__init__.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.signals import user_logged_in
+
+from django_agent_trust import trust_agent, trust_session
+
+
+def _handle_auth_login(sender, request, user, **kwargs):
+    """
+    Automatically establishes agent trust if indicated by one of our
+    authentication forms.
+    """
+    if getattr(user, 'otp_trust_this_agent', False):
+        trust_agent(request)
+    elif getattr(user, 'otp_trust_this_session', False):
+        trust_session(request)
+
+
+user_logged_in.connect(_handle_auth_login)
