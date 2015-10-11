f3db0425ce958a1230f10213af902ac5bf9b7964
Peter Sagerson <psagers@ignorare.net>
2015-10-11 13:45:12 -0700

    Updated admin templates, test cleanup, and general forward compatibility.

--- otp_agents/__init__.py
+++ otp_agents/__init__.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.signals import user_logged_in
 
 from django_agent_trust import trust_agent, trust_session
-from django_agent_trust.models import AgentSettings
 
 
 def _handle_auth_login(sender, request, user, **kwargs):
@@ -9,6 +8,8 @@ def _handle_auth_login(sender, request, user, **kwargs):
     Automatically establishes agent trust if indicated by one of our
     authentication forms.
     """
+    from django_agent_trust.models import AgentSettings
+
     AgentSettings.objects.get_or_create(user=user)
 
     if getattr(user, 'otp_trust_this_agent', False):
--- otp_agents/test/urls.py
+++ otp_agents/test/urls.py
@@ -1,15 +1,17 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+import django.contrib.auth.views
+
+import django_otp.views
+import otp_agents.views
 
 from . import views
 
 
-urlpatterns = patterns(
-    '',
-
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^verify/$', 'django_otp.views.login'),
-    url(r'^trust/$', 'otp_agents.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+urlpatterns = [
+    url(r'^login/$', django.contrib.auth.views.login),
+    url(r'^verify/$', django_otp.views.login),
+    url(r'^trust/$', otp_agents.views.login),
+    url(r'^logout/$', django.contrib.auth.views.logout),
 
     url(r'^otp/$', views.otp_view),
     url(r'^otp2/$', views.otp_view_2),
@@ -17,4 +19,4 @@ urlpatterns = patterns(
     url(r'^otp_advised/$', views.otp_advised_view),
     url(r'^otp_advised_2/$', views.otp_advised_view_2),
     url(r'^agent/$', views.agent_view),
-)
+]
