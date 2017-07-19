a8f9ebdcf4505c22ba181cb7b06d20be07a2d294
Peter Sagerson <psagers@ignorare.net>
2017-07-19 09:51:09 -0700

    Drop support for old versions of Python and Django.
    All minor version numbers have been bumped.

--- README.rst
+++ README.rst
@@ -5,7 +5,7 @@ django-otp.
 
 See `django-otp`_ for more information on the OTP framework.
 
-This version is supported on Python 2.6, 2.7, and 3.3+; and Django >= 1.4.
+This version is supported on Python 2.7 and 3.4+; and Django 1.8 and 1.10+.
 
 
 .. _django-agent-trust: http://pypi.python.org/pypi/django-agent-trust
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='django-otp-agents',
-    version='0.2.6',
+    version='0.3.0',
     description="Integration of django-otp and django-agent-trust.",
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
@@ -19,18 +19,17 @@ setup(
     url='https://bitbucket.org/psagers/django-otp',
     license='BSD',
     install_requires=[
-        'django-otp >= 0.2.0',
+        'django-otp >= 0.4.0',
         'django-agent-trust >= 0.1.8',
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Security",
